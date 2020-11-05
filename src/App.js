cf97d0e59d73015c35e45c910662426b82e5a718
Kundan Kumar <kdn.rthr@gmail.com>
2020-11-06 02:57:51 +0530

    first react app commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, {useState,useEffect}  from 'react';
 import './App.css';
+import Header from './Components/Header';
+import Recipe from './Components/Recipe'
+import Axios from 'axios'
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [recipe, setRecipe] = useState([]);
+
+  const APP_ID ="320e8c75";
+  const APP_KEY ="f8f8fd5b8e864689ee92053a4e818a3c";
+
+  useEffect(()=>{
+    getRecipe();
+  }, []);
+
+  const getRecipe = async() => {
+    const res = await Axios.get(
+      `https://api.edamam.com/search?q=chiken&app_id=${APP_ID}&app_key=${APP_KEY}`
+     );
+     setRecipe(res.data.hits)
+    }
+
+  const onInputChange =(e) => {
+     setSearch(e.target.value)
+    //  console.log(e.target.value)
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Header search={search} onInputChange={onInputChange} />
+    <div className = 'container'>
+    <Recipe recipe = {recipe} />
+    </div>
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+// function Header (){
+//     return(
+//         <hi>Header</hi>
+//     )
+// }
+const Header = (props) => {
+    const {search, onInputChange} = props;
+    return( 
+        <div className= 'jumbotron'>
+        <h1 className = 'display-1'><i className="material-icons brand-icon">
+             fastfood</i>Food Recipe</h1>
+             <div className="input-group w-50 mx-auto ">
+            <input type="text" className="form-control" 
+             placeholder = 'Search Your Recipe...'
+             value= {search}
+             onChange= {onInputChange}
+             />
+            <div className="input-group-append">
+            <button className = 'btn btn-dark'>Search Recipe</button>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
